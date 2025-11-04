c9221d807e87431d41932a8036cee8d22aaeaced
bornac1 <borna.cesarec1@gmail.com>
2025-11-04 20:27:40 +0100

    Remove debuging

--- HorizontalView.cs
+++ HorizontalView.cs
@@ -139,7 +139,7 @@ namespace AuroraPAR
             Canvas.Children.Add(elipse);
             TextBlock textBlock = new()
             {
-                Text = $"{aircraft.Callsign}\n{aircraft.Altitude}\n\n{(aircraft.BearingFromRunway(runway) - runway.Heading + 360) % 360 - 180}°",
+                Text = $"{aircraft.Callsign}\n{aircraft.Altitude}",
                 FontSize = 12,
                 Foreground = Brushes.White
             };
